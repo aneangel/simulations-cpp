c37b87c1f6b8b7f8d9ab2279de49640f54adc56b
Anthony Angeles <aangeles0280@gmail.com>
2024-11-30 13:11:07 -0800

    update: simulations cpp; revisitng project with CUDA

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <GL/glut.h>
+#include "ParticleFactory.h"
+#include "KernelFactory.h"
+#include "ForceFactory.h"
+#include "BoundaryFactory.h"
